dc050d9ce748b2d818322c07e62d95a1f4c4ee9c
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-04 11:13:14 -0800

    Add support for Groovy (files needs to be in a git repo in order for the lsp to attach)

---
+++ lua/plugins/groovy.lua
@@ -0,0 +1,20 @@
+return {
+  -- Add groovy to treesitter.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "groovy" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        groovyls = {},
+      },
+    },
+  },
+}
