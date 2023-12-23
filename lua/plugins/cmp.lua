72fe77236536841f7d5052c3aa2d66451eca88bd
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-22 23:12:35 -0800

    Tab for completion with replace and not insert

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<tab>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+      ["<Tab>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
     })
   end,
 }
