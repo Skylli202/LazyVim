43608ac29461b87e6f251b3c00ac45640d7a8e35
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-28 01:19:52 -0800

    idk what im doing its late

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,5 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
+  { "rcarriga/nvim-notify", enabled = true },
   { "echasnovski/mini.pairs", enabled = true },
   { "saadparwaiz1/cmp_luasnip", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enable = false },
-  { "RRethy/vim-illuminate", enable = false },
-  { "nvim-lualine/lualine.nvim", enable = false },
 }
