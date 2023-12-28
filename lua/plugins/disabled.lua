4d813d26c80f15afdf2ae4ce6fc6fd63312d3f8a
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-28 01:03:26 -0800

    disabled a bunch of stuff

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,8 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.pairs", enabled = true },
   { "saadparwaiz1/cmp_luasnip", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enable = false },
+  { "RRethy/vim-illuminate", enable = false },
+  { "nvim-lualine/lualine.nvim", enable = false },
 }
