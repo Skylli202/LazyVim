577c69c0a4a60198ca8e2f54c3927d6efdb36bec
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-28 21:49:06 -0800

    added various language for TreeSitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add tsx and treesitter
     vim.list_extend(opts.ensure_installed, {
+      "bash",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
       "typescript",
       "groovy",
+      "regex",
+      "xml",
     })
   end,
 }
