19d63026510bbe1ff06ae3036879321339b72d69
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-01-05 19:42:39 -0800

    add vscode

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.vscode" },
 
     -- import/override with your plugins
     { import = "plugins" },
