0f4b224fd8b3d1df45d20664e557451fff6c54e4
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-01-26 11:09:52 -0800

    added netwe

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 -- Keymaps are automatically loaueu on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+map("n", "<leader>pv", ":Explore<CR>", { desc = "Open netwr" })
 vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
