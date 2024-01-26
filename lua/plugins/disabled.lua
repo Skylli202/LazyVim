fe8c6721e7f37a340199ecc0dc6fe72db439d1d3
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-01-26 11:09:06 -0800

    disabled pairs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.pairs", enabled = true },
+  { "echasnovski/mini.pairs", enabled = false },
   { "saadparwaiz1/cmp_luasnip", enabled = false },
   { "folke/noice.nvim", enabled = false },
 }
