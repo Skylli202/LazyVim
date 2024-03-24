6955999a05d8fabdb5553b472869024bb0bc2902
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-23 20:38:58 -0700

    renabled all LazyVim plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-  { "saadparwaiz1/cmp_luasnip", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = true },
+  { "echasnovski/mini.pairs", enabled = true },
+  { "saadparwaiz1/cmp_luasnip", enabled = true },
+  { "folke/noice.nvim", enabled = true },
 }
