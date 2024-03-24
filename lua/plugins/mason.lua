e3c309da06cd4f2c5bb8a5d9aa4434437365df54
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-23 20:39:31 -0700

    added svelte lsp in ensure_installed table

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,7 @@ return {
     "xilliamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
+        "svelte",
         "templ",
       })
       -- vim.filetype.add({
