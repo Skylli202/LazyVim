df36728917c914a344e3870c6810caf5b45760a3
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-23 20:46:37 -0700

    reverse svelte from ensured_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,6 @@ return {
     "xilliamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "svelte",
         "templ",
       })
       -- vim.filetype.add({
