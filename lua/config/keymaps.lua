07a2f9b2b7fbbc3c78e0ac4a9d808a88fd664c72
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-23 20:38:47 -0700

    enabled back C-S

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,4 +7,4 @@ local map = Util.safe_keymap_set
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 map("n", "<leader>pv", ":Explore<CR>", { desc = "Open netwr" })
-vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
+-- vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
