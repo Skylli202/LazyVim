1f4c22f85792def913be423567aaa72d11f33e59
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-07-28 20:26:25 -0700

    idk what the actual fuck is with luasnip but there is one

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
-  { "saadparwaiz1/cmp_luasnip", enabled = false },
   { "folke/noice.nvim", enabled = true },
 }
